255055d8f2c5304e19c8ac1747cb5fb81dd45b7f
Simon McGuirk <simon.mcguirk@outlook.com>
2024-02-20 23:52:02 +0000

    Complete Day 02 tutorials

---
+++ Day02/Checkpoint1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let tempInCelcius = 21.0
+let tempInFahrenheit = ((tempInCelcius * 9) / 5) + 32
+print("Temperature: \(tempInCelcius)°C | \(tempInFahrenheit)°F")
